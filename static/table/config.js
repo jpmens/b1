c4dd09220b38f574aa6c846c0205e1aee1aaff1a
Jan-Piet Mens <jpmens@gmail.com>
2014-08-28 20:02:24 +0200

    w00t! table in too

---
+++ static/table/config.js
@@ -0,0 +1,9 @@
+var config = {
+	websockethost:	'172.16.153.122',
+	websocketport:	9001,
+	// websockethost:	'tiggr.ww.mens.de',
+	// websocketport:	9010,
+	usetls:		false,
+	subscribelist:  [ 't/+/+' ],
+	topic_visible: 	false,		// hide/show topic in first col of table
+};
